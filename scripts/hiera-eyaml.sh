fa7f0803c3cfafe4c4d06b9ae28a5a47b9d0fe08
Sebastian Otaegui <feniix@gmail.com>
2014-03-31 22:12:31 -0500

    Add hiera-eyaml + hiera-eyaml-gpg

---
+++ scripts/hiera-eyaml.sh
@@ -0,0 +1,7 @@
+apt-get install -y libgpgme11-dev
+gem install -V hiera-eyaml 
+gem install -V gpgme -- --use-system-libraries
+gem install -V hiera-eyaml-gpg
+gem install -V hiera-eyaml-plaintext
+
+sleep 3
---
+++ scripts/vmware.sh
@@ -0,0 +1,13 @@
+apt-get -y install --no-install-recommends libdbus-1-3
+
+# The netboot installs the VirtualBox support (old) so we have to remove it
+/etc/init.d/virtualbox-ose-guest-utils stop
+rmmod vboxguest
+aptitude -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
+aptitude -y install dkms
+
+mount -t iso9660 -o loop "linux.iso" /mnt
+tar xzvf /mnt/VMware*.tar.gz -C /tmp
+cd /tmp/vmware-tools-distrib/
+./vmware-install.pl -d
+
