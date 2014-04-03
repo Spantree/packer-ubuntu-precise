da84979c6b7ad8bdbcbdcfdca0123fb9f00621ab
Sebastian Otaegui <feniix@gmail.com>
2014-04-02 19:53:01 -0500

    do cleanup-ifs only on vbox and vmware and use the gem gpgme with its own deps

--- scripts/hiera-eyaml.sh
+++ scripts/hiera-eyaml.sh
@@ -1,6 +1,6 @@
 apt-get install -y libgpgme11-dev
 gem install -V hiera-eyaml 
-gem install -V gpgme -- --use-system-libraries
+gem install -V gpgme
 gem install -V hiera-eyaml-gpg
 gem install -V hiera-eyaml-plaintext
 
